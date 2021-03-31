80a43de6855ed90eeb1ac5328b7c970d08b94638
lvv121 <maticzar5@gmail.com>
2021-03-31 23:58:29 +0200

    C# files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.heart1 = new System.Windows.Forms.PictureBox();
+            this.heart2 = new System.Windows.Forms.PictureBox();
+            this.heart3 = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.manLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.heart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textScore
+            // 
+            this.textScore.AutoSize = true;
+            this.textScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textScore.ForeColor = System.Drawing.Color.White;
+            this.textScore.Location = new System.Drawing.Point(12, 525);
+            this.textScore.Name = "textScore";
+            this.textScore.Size = new System.Drawing.Size(89, 24);
+            this.textScore.TabIndex = 1;
+            this.textScore.Text = "Wynik: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTime);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(482, 525);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Życia: ";
+            // 
+            // heart1
+            // 
+            this.heart1.Image = global::Projekt.Properties.Resources.heart;
+            this.heart1.Location = new System.Drawing.Point(560, 521);
+            this.heart1.Name = "heart1";
+            this.heart1.Size = new System.Drawing.Size(50, 28);
+            this.heart1.TabIndex = 5;
+            this.heart1.TabStop = false;
+            // 
+            // heart2
+            // 
+            this.heart2.Image = global::Projekt.Properties.Resources.heart;
+            this.heart2.Location = new System.Drawing.Point(616, 521);
+            this.heart2.Name = "heart2";
+            this.heart2.Size = new System.Drawing.Size(50, 28);
+            this.heart2.TabIndex = 4;
+            this.heart2.TabStop = false;
+            // 
+            // heart3
+            // 
+            this.heart3.Image = global::Projekt.Properties.Resources.heart;
+            this.heart3.Location = new System.Drawing.Point(672, 521);
+            this.heart3.Name = "heart3";
+            this.heart3.Size = new System.Drawing.Size(50, 28);
+            this.heart3.TabIndex = 2;
+            this.heart3.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Projekt.Properties.Resources._1;
+            this.player.Location = new System.Drawing.Point(302, 455);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 94);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // manLabel
+            // 
+            this.manLabel.AutoSize = true;
+            this.manLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.manLabel.Location = new System.Drawing.Point(12, 111);
+            this.manLabel.Name = "manLabel";
+            this.manLabel.Size = new System.Drawing.Size(88, 20);
+            this.manLabel.TabIndex = 7;
+            this.manLabel.Text = "Instrukcja";
+            this.manLabel.Click += new System.EventHandler(this.manLabelClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.manLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.heart1);
+            this.Controls.Add(this.heart2);
+            this.Controls.Add(this.heart3);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.textScore);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.heart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heart3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Label textScore;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox heart3;
+        private System.Windows.Forms.PictureBox heart2;
+        private System.Windows.Forms.PictureBox heart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label manLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using Projekt.Properties;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox[] enemiesArray;
+        private int enemyBulletTimer = 100;
+        private int enemySpeed = 5;
+        private bool goLeft, goRight;
+        private bool isGameOver;
+        private readonly int playerSpeed = 12;
+        private int score;
+
+        private bool shooting;
+
+        public Form1()
+        {
+            InitializeComponent();
+            gameSetup();
+        }
+
+        private void gameTime(object sender, EventArgs e)
+        {
+            textScore.Text = "Wynik: " + score;
+            if (goLeft)
+                player.Left -= playerSpeed;
+
+            if (goRight)
+                player.Left += playerSpeed;
+
+            enemyBulletTimer -= 10;
+
+            if (enemyBulletTimer < 1)
+            {
+                enemyBulletTimer = 300;
+                makeBullet("enemyBullet");
+            }
+
+            foreach (Control x in Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "enemies")
+                {
+                    x.Left += enemySpeed;
+
+                    if (x.Left > 730)
+                    {
+                        x.Top += 65;
+                        x.Left = -80;
+                    }
+
+                    if (x.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        heart1.Image = null;
+                        heart2.Image = null;
+                        heart3.Image = null;
+                        gameOver("Zostałeś pokonany!");
+                    }
+
+                    foreach (Control y in Controls)
+                        if (y is PictureBox && (string)y.Tag == "bullet")
+                            if (y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                Controls.Remove(x);
+                                Controls.Remove(y);
+                                score += 1;
+                                shooting = false;
+                            }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "bullet")
+                {
+                    x.Top -= 20;
+
+                    if (x.Top < 15)
+                    {
+                        Controls.Remove(x);
+                        shooting = false;
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "enemyBullet")
+                {
+                    x.Top += 20;
+
+                    if (x.Top > 620) Controls.Remove(x);
+
+                    if (heart1.Image != null && x.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        Controls.Remove(x);
+                        heart1.Image = null;
+                    }
+                    else if (heart2.Image != null && x.Bounds.IntersectsWith(player.Bounds) && heart1.Image == null)
+                    {
+                        Controls.Remove(x);
+                        heart2.Image = null;
+                    }
+                    else if (heart3.Image != null && x.Bounds.IntersectsWith(player.Bounds) && heart1.Image == null &&
+                             heart2.Image == null)
+                    {
+                        Controls.Remove(x);
+                        heart3.Image = null;
+                        gameOver("Skończyły ci się życia!");
+                    }
+                }
+            }
+
+            if (score > 8) enemySpeed = 10;
+
+            if (score == enemiesArray.Length) gameOver("Wygrana!");
+        }
+
+        private void keyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                goLeft = true;
+
+            if (e.KeyCode == Keys.Right)
+                goRight = true;
+        }
+
+        private void keyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                goLeft = false;
+
+            if (e.KeyCode == Keys.Right)
+                goRight = false;
+
+            if (e.KeyCode == Keys.Space && shooting == false)
+            {
+                shooting = true;
+                makeBullet("bullet");
+            }
+
+            if (e.KeyCode == Keys.Enter && isGameOver)
+            {
+                removeAll();
+                gameSetup();
+            }
+        }
+
+        private void makeEnemies()
+        {
+            enemiesArray = new PictureBox[15];
+
+            var left = 0;
+
+            for (var i = 0; i < enemiesArray.Length; i++)
+            {
+                enemiesArray[i] = new PictureBox();
+                enemiesArray[i].Size = new Size(60, 50);
+                enemiesArray[i].Image = Resources._2;
+                enemiesArray[i].Top = 5;
+                enemiesArray[i].Tag = "enemies";
+                enemiesArray[i].Left = left;
+                enemiesArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                Controls.Add(enemiesArray[i]);
+                left = left - 80;
+            }
+        }
+
+        private void gameSetup()
+        {
+            textScore.Text = "Wynik: 0";
+            score = 0;
+            isGameOver = false;
+
+            enemyBulletTimer = 300;
+            enemySpeed = 5;
+            shooting = false;
+
+            manLabelText();
+            makeEnemies();
+            gameTimer.Start();
+        }
+
+        private void gameOver(string message)
+        {
+            isGameOver = true;
+            gameTimer.Stop();
+
+            var result = (MessageBoxResult)MessageBox.Show(
+                message + "\nTwój wynik to " + score + "\nCzy chciałbyś zagrać jeszcze?", "Koniec gry",
+                (MessageBoxButtons)MessageBoxButton.YesNoCancel);
+            switch (result)
+            {
+                case MessageBoxResult.Yes:
+                    removeAll();
+                    gameSetup();
+                    break;
+
+                case MessageBoxResult.No:
+                    Close();
+                    break;
+
+                case MessageBoxResult.Cancel:
+                    Close();
+                    break;
+            }
+        }
+
+        private void removeAll()
+        {
+            foreach (var i in enemiesArray) Controls.Remove(i);
+
+            foreach (Control x in Controls)
+                if (x is PictureBox)
+                    if ((string)x.Tag == "bullet" || (string)x.Tag == "enemyBullet")
+                        Controls.Remove(x);
+        }
+
+        private void makeBullet(string bulletTag)
+        {
+            var bullet = new PictureBox();
+            bullet.Image = Resources.bullet;
+            bullet.Size = new Size(5, 20);
+            bullet.Tag = bulletTag;
+            bullet.Left = player.Left + player.Width / 2;
+
+            if ((string)bullet.Tag == "bullet")
+                bullet.Top = player.Top - 20;
+            else if ((string)bullet.Tag == "enemyBullet") bullet.Top = 100;
+
+            Controls.Add(bullet);
+            bullet.BringToFront();
+        }
+
+        private void manLabelText()
+        {
+            manLabel.Text =
+                "Instrukcja\nstrzałka w lewo -> ruch statku w lewo\nstrzałka w prawo -> ruch statku w prawo\nspacja -> wystrzelenie pocisku przez statek\nKliknij, aby to okienko z tekstem zniknęło";
+        }
+
+        private void manLabelClick(object sender, EventArgs e)
+        {
+            manLabel.Text = "";
+        }
+    }
+}
